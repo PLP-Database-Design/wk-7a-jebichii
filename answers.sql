fcb64e7e9cbf377e9d759649c1fcf71f627bc541
jebichii <jebichiijoyce@gmail.com>
2025-05-07 10:45:05 +0300

    normalization

--- answers.sql
+++ answers.sql
@@ -1,44 +1,45 @@
 
 -- QUESTION 1
 -- Transform into 1NF
-SELECT OrderID, CustomerName, Product
-FROM (
-    VALUES
-        (101, 'John Doe', 'Laptop'),
-        (101, 'John Doe', 'Mouse'),
-        (102, 'Jane Smith', 'Tablet'),
-        (102, 'Jane Smith', 'Keyboard'),
-        (102, 'Jane Smith', 'Mouse'),
-        (103, 'Emily Clark', 'Phone')
-) AS NormalizedTable (OrderID, CustomerName, Product);
--- QUESTION 2
--- Create Orders Table
-CREATE TABLE Orders (
+CREATE TABLE ProductDetail(
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID,CustomerName,Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+-- Question 2
+CREATE TABLE Orders(
     OrderID INT PRIMARY KEY,
-    CustomerName VARCHAR(255)
+    CustomerName VARCHAR(100)
 );
 
-INSERT INTO Orders (OrderID, CustomerName)
+INSERT INTO Orders(OrderID,CustomerName)
 VALUES
-    (101, 'John Doe'),
-    (102, 'Jane Smith'),
-    (103, 'Emily Clark');
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
--- Create OrderDetails Table
-CREATE TABLE OrderDetails (
+
+CREATE TABLE Product(
     OrderID INT,
-    Product VARCHAR(255),
+    Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
-
-INSERT INTO OrderDetails (OrderID, Product, Quantity)
+INSERT INTO Product (OrderID,Product,Quantity)
 VALUES
-    (101, 'Laptop', 2),
-    (101, 'Mouse', 1),
-    (102, 'Tablet', 3),
-    (102, 'Keyboard', 1),
-    (102, 'Mouse', 2),
-    (103, 'Phone', 1);
-
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
